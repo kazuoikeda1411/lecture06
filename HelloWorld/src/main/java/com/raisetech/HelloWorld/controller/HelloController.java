cc5e6fa623ffa570c25f6972d6165470835d4628
kazuoikeda1411 <erizabesu1411@icloud.com>
2022-11-28 13:30:44 +0900

    第６講課題①

---
+++ HelloWorld/src/main/java/com/raisetech/HelloWorld/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.raisetech.HelloWorld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	@GetMapping ("/hello")
+	public String hello() {
+		return "index";
+	}
+}
